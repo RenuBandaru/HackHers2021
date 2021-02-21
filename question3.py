993313f3fb3152348fc33a6778b8bf3ed40f8052
RenuBandaru <renusree.bandaru@gmail.com>
2021-02-21 00:03:35 -0500

    IT WORKS

--- question3.py
+++ question3.py
@@ -5,15 +5,32 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 vaccinations_df = pd.read_csv("vaccinations.csv")
-result=pd.pivot_table(vaccinations_df,index=["location"],values=["total_vaccinations"],aggfunc='sum')
-print(result.head(30))
 
-# grouped = vaccinations_df.groupby(['location', 'total_vaccinations'])
-# print(grouped[['location']].aggregate({'Open':np.mean, 'Volume':np.min}))
+#vaccinations_df.groupby(['location']).tail(1)
 
-vaccinations_df.groupby(['location']).tail(1)
+print('----------------------------------------------- \n\n')
+country_pop_df = pd.read_csv("covid19_country_population.csv")
+pd.set_option('display.max_rows', country_pop_df.shape[0]+1)
+print(country_pop_df)
 
-result=pd.pivot_table(vaccinations_df,index=["location"],values=["total_vaccinations"],aggfunc='sum')
-print(vaccinations_df)
+print('----------------------------------------\n\n')
+#merge df with iso_df so we have the iso code for the countries listed:
+df=pd.merge(vaccinations_df, country_pop_df, left_on="iso_code", right_on="CountryAlpha3Code")  #merge county an survey on fibs
+#df.drop(['Alpha-2 code','Alpha-3 code', 'Numeric code', 'ISO 3166-2'], axis=1, inplace=True)
+#pd.set_option('display.max_rows', df.shape[0]+1)
 
-country_pop_df = pd.read_csv("")
+df['percentVac'] = df['total_vaccinations']/df['population']
+
+
+#df=pd.merge(result, country_pop_df, left_on="iso_code", right_on="CountryAlpha3Code") 
+#result=pd.pivot_table(vaccinations_df,index=["location","iso_code"],values=["total_vaccinations"],aggfunc='max')
+#result=pd.pivot_table(vaccinations_df,index=["location"],values=["daily_vaccinations"],aggfunc='sum')
+result=pd.pivot_table(df,index=["location", "iso_code","population"],values=["total_vaccinations","percentVac"],aggfunc='max')
+
+pd.set_option('display.max_rows', result.shape[0]+1)
+print(result)
+
+print('----------------------------------------------- \n')
+
+
+
